c82069db26f240e99cc1e7e4e0b91d72982ab7cd
LazyEngineerToBe <alexandre.francoeur.3@etsmtl.net>
2019-08-30 18:55:25 -0400

    removed double include.

--- include/WMKinovaHardwareInterface.h
+++ include/WMKinovaHardwareInterface.h
@@ -8,7 +8,6 @@
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
-#include <hardware_interface/joint_command_interface.h>
 
 #include <diagnostic_msgs/DiagnosticStatus.h>
 #include <diagnostic_msgs/DiagnosticArray.h>
